300d3e7f47336519b61f637eaeba37e5aa90f518
anandrajt1 <140165641+anandrajt1@users.noreply.github.com>
2023-09-17 17:26:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import TaskManager from './components/TaskManager';
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <TaskManager></TaskManager>
+      
+    </div>
+  );
+}
+
+export default App;
